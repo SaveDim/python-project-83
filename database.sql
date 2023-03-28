e00b601793ab04c1e4863d1882f45576ba7fcab8
DS <jieto35@yandex.ru>
2023-03-28 13:34:19 +0300

    Update tables

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS urls
 
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(255) UNIQUE NOT NULL,
   created_at DATE DEFAULT now()
@@ -8,12 +8,12 @@ CREATE TABLE urls (
 
 DROP TABLE IF EXISTS url_checks
 
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS url_checks (
   id SERIAL PRIMARY KEY,
   url_id bigint REFERENCES urls(id),
   status_code SMALLINT,
   h1 VARCHAR(255),
   title VARCHAR(255),
   description VARCHAR(255),
-  created_at DATE NOT NULL
+  created_at DATE NOT NULL DEFAULT now()
 );
