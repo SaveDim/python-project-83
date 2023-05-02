4cd0d97b68f6a882fc27c0ad7196b26bacedd4ad
savedim <savedimdev@gmail.com>
2023-05-02 14:37:08 +0300

    Fix linter

--- page_analyzer/url_parser.py
+++ page_analyzer/url_parser.py
@@ -2,6 +2,10 @@ from urllib.parse import urlparse
 
 
 def parser(url):
-    url_parsed = urlparse(url)._replace(fragment="", params="", query="", path="").geturl()
+    url_parsed = (
+        urlparse(url)
+        ._replace(fragment="", params="", query="", path="")
+        .geturl()
+    )
     url_parsed = url_parsed.replace("www.", "")
     return url_parsed
